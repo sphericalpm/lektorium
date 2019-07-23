e7d1c1790bf294f910e873696adfa0241e1f4588
Katridi <katridi@yandex.ru>
2019-07-23 15:44:17 +0300

    naming fixes

--- legit.py
+++ legit.py
@@ -1,6 +1,7 @@
 from git import Repo
 from git.exc import GitCommandError, InvalidGitRepositoryError
 from typing import Iterable
+import logging
 
 # parent_commits: Tuple[str, str] = (), author: Tuple[str, str] = author,
 # committer: Tuple[str, str] = committer
@@ -11,6 +12,7 @@ from typing import Iterable
 
 
 class Repository:
+
 
     def __init__(self, path: str) -> None:
         self.repo = Repo.init(path)
@@ -29,8 +31,8 @@ class Repository:
         except GitCommandError:
             self.repo.git.checkout(branch_name)
 
-    def commit(self, meaningful_msg: str) -> None:
-        self.repo.index.commit(meaningful_msg)
+    def commit(self, message: str) -> None:
+        self.repo.index.commit(message)
 
     def add_changes(self, files: Iterable[str] = ()) -> None:
         [self.repo.index.add(file) for file in files] or self.repo.git.add(A=True)
@@ -41,12 +43,10 @@ class Repository:
         self.repo.git.pull("origin", branch_name)
         self.repo.git.push("origin", 'HEAD:' + branch_name)
 
-    def merge_branch(self, branch_name: str, msg_for_merging: str, parent_branch: str = 'master') -> None:
-        current = self.repo.branches[branch_name]
-        master = self.repo.branches[parent_branch]
-        merge_base = self.repo.merge_base(branch_name, master)
-        self.repo.index.merge_tree(master, base=merge_base)
-        self.commit(msg_for_merging)
-        # At this point, we have successfully merged the two branches
-        # but we have not modified the working directory.
-        current.checkout(force=True)
+    def merge_branch(self, merge_from: str,  merge_into: str,  message_for_merging: str) -> None:
+        branch_from = self.repo.branches[merge_from]
+        branch_into = self.repo.branches[merge_into]
+        merge_base = self.repo.merge_base(merge_from, branch_into)
+        self.repo.index.merge_tree(branch_into, base=merge_base)
+        self.commit(message_for_merging)
+        branch_from.checkout(force=True)
