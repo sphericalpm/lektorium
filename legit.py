8063789df776745d823e93058daac889299b6a1a
Katridi <katridi@yandex.ru>
2019-07-23 14:53:05 +0300

    space

--- legit.py
+++ legit.py
@@ -23,7 +23,7 @@ class Repository:
             Repo.clone_from(url, path)
         return cls(path)
 
-    def create_branch(self, branch_name: str) -> None: 
+    def create_branch(self, branch_name: str) -> None:
         try:
             self.repo.git.checkout('-b', branch_name)
         except GitCommandError:
