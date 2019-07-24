c313e7848faaf012e79383e0d231766e096f55df
Sergey Kozlov <serg@zillent.ru>
2019-07-24 23:14:41 +0500

    created site config and modules prototype

---
+++ config/config.py
@@ -0,0 +1,3 @@
+import ruamel.yaml as yaml
+
+
---
+++ config/tests.py
@@ -0,0 +1,10 @@
+import unittest
+
+class TestSiteConfig(unittest.TestCase):
+    def test_check_config(self):
+        self.assertEqual(1,1)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
