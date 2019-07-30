607daa47698d183fdc74b1e11856680834f5a87a
Asja Makarowska <anutya@mail.ru>
2019-07-30 17:09:36 +0300

    Delete garbage collector + commit 8th case (small fixes before the call)

--- tests/test_repo_legit.py
+++ tests/test_repo_legit.py
@@ -1,19 +1,16 @@
-from os.path import expanduser, isdir, exists, join, abspath, dirname
+from os.path import expanduser, isdir, exists, join
 from os import makedirs, chmod, walk
 from git import Repo
-import sys
 import legit
 from shutil import rmtree
 import unittest
 
 url = 'https://github.com/katridi/test_folder.git'
-folder = join(expanduser('~'), 'Desktop\\folder2')
-path = abspath(dirname(dirname(abspath(__file__))))
-sys.path.append(path)
+folder = join(expanduser('~'), 'Desktop\\folder2') #TODO Change
 
 
 def build_path(sub_dir: str) -> str:
-    return join(expanduser('~'), f"Desktop\\{sub_dir}")
+    return join(expanduser('~'), f"Desktop\\{sub_dir}") #TODO Change
 
 
 def create_folder(folder: str) -> str:
@@ -45,57 +42,46 @@ def clean_and_create(folder:str):
 
 class TestGitMethods(unittest.TestCase):
 
-    def tearDown(self):
-        import gc
-        gc.collect()
-
-    def test_some(self):
-        r = create_folder('./123')
-        clean_folders(r)
-
     def test_initialization(self):
-        clean_folders(folder)
-        repository = legit.Repository(create_folder(folder))
+        #clean_folders(folder)
+        repository = legit.Repository(create_folder('folder'))
         repo = repository.repo
         assert isdir(repo.working_tree_dir)
         assert repo.git_dir.startswith(repo.working_tree_dir)
 
     def test_initialization_dot_attr(self):
-        clean_folders(folder)
-        repository = legit.Repository.init(create_folder(folder))
+        #clean_folders(folder)
+        repository = legit.Repository.init(create_folder('folder'))
         repo = repository.repo
         assert isdir(repo.working_tree_dir)
         assert repo.git_dir.startswith(repo.working_tree_dir)
 
     def test_cloning_empty_dir(self):
-        clean_folders(folder)
-        repository = legit.Repository(create_folder(folder)).repo
+        #clean_folders(folder)
+        repository = legit.Repository(create_folder('folder')).repo
         repo = Repo(repository.working_tree_dir)
         cloned_repo = repo.clone(clean_and_create(build_path('newrepo')))  # TODO Check?
         # clone an existing repository
         assert cloned_repo.__class__ is Repo
         assert Repo.init(folder).__class__ is Repo
-        # directory with your work files
         assert isdir(cloned_repo.working_tree_dir)
         # directory containing the git repository
         assert cloned_repo.git_dir.startswith(cloned_repo.working_tree_dir)
 
     def test_cloning_non_empty_dir(self):
         cloned_repo = legit.Repository.clone(url, clean_and_create(build_path('newrepo')))  # TODO Check?
-        # no working tree
         git_path = build_path('newrepo\\.git')
         assert isdir(git_path)
         self.assertEqual(git_path, cloned_repo.repo.git_dir)
 
     def test_branching(self):
-        cloned_repo = legit.Repository.clone(url, clean_and_create(build_path('newrepo'))).repo  # TODO Check?
+        cloned_repo = legit.Repository.clone(url, clean_and_create('newrepo')).repo  # TODO Check?
         # create a new branch
         new_branch = cloned_repo.create_head('feature')
-        # which wasn't checked out yet
         assert cloned_repo.active_branch != new_branch
 
     def test_commiting(self):
-        cloned_repo = legit.Repository.clone(url, clean_and_create(build_path('newrepo'))).repo  # TODO Check?
+        cloned_repo = legit.Repository.clone(url, clean_and_create('newrepo')).repo  # TODO Check?
         new_branch = cloned_repo.create_head('another-branch')
         assert cloned_repo.active_branch != new_branch
         # pointing to the checked-out commit
@@ -110,28 +96,28 @@ class TestGitMethods(unittest.TestCase):
         r.index.add([file_name])
         r.index.commit("initial commit")
 
-    def test_push(self):
-        repository = legit.Repository.init(create_folder(folder)).repo
-        repo = legit.Repository.init(folder).repo
-        assert isdir(repository.working_tree_dir)
-        assert repository.git_dir.startswith(repository.working_tree_dir)
-        empty_repo = Repo.init(join(folder, 'empty'))
-        origin = empty_repo.create_remote('origin', repo.remotes.origin.url)
-        assert origin.exists()
-        assert origin == empty_repo.remotes.origin == empty_repo.remotes['origin']
-        # assure we actually have data. fetch() returns useful information
-        origin.fetch()
-        # Setup a local tracking branch of a remote branch
-        # create local branch "master" from remote "master"
-        empty_repo.create_head('master', origin.refs.master)
-        # set local "master" to track remote "master
-        empty_repo.heads.master.set_tracking_branch(origin.refs.master)
-        # checkout local "master" to working tree
-        empty_repo.heads.master.checkout()
-        empty_repo.create_head('master', origin.refs.master).set_tracking_branch(origin.refs.master).checkout()
-        # rename remotes
-        origin.rename('new_origin')
-        origin.pull()
-        origin.push()
-        # create and delete remotes
-        assert not empty_repo.delete_remote(origin).exists()
+    # def test_push(self):
+    #     repository = legit.Repository.init(create_folder(folder)).repo
+    #     repo = legit.Repository.init(folder).repo
+    #     assert isdir(repository.working_tree_dir)
+    #     assert repository.git_dir.startswith(repository.working_tree_dir)
+    #     empty_repo = Repo.init(join(folder, 'empty'))
+    #     origin = empty_repo.create_remote('origin', repo.remotes.origin.url)
+    #     assert origin.exists()
+    #     assert origin == empty_repo.remotes.origin == empty_repo.remotes['origin']
+    #     # assure we actually have data. fetch() returns useful information
+    #     origin.fetch()
+    #     # Setup a local tracking branch of a remote branch
+    #     # create local branch "master" from remote "master"
+    #     empty_repo.create_head('master', origin.refs.master)
+    #     # set local "master" to track remote "master
+    #     empty_repo.heads.master.set_tracking_branch(origin.refs.master)
+    #     # checkout local "master" to working tree
+    #     empty_repo.heads.master.checkout()
+    #     empty_repo.create_head('master', origin.refs.master).set_tracking_branch(origin.refs.master).checkout()
+    #     # rename remotes
+    #     origin.rename('new_origin')
+    #     origin.pull()
+    #     origin.push()
+    #     # create and delete remotes
+    #     assert not empty_repo.delete_remote(origin).exists()
