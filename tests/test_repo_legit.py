09e92321471d3b875060631e4b23705a21dcbcd5
Asja Makarowska <anutya@mail.ru>
2019-08-22 20:14:14 +0300

    rewrite test_add_file_and_commit (check if committed file is in git) + small fixes (clean code, rewrite test_clone_into_empty_dir, switched from TemporaryDirectory, TemporaryFile to NamedTemporaryFile)

--- tests/test_repo_legit.py
+++ tests/test_repo_legit.py
@@ -1,13 +1,11 @@
+from os import sep
+from tempfile import NamedTemporaryFile
 from os.path import isdir
 from tempfile import TemporaryDirectory, TemporaryFile
 
 from git import Repo
-
-import legit
 import pytest
-
-
-# import mockssh
+import legit
 
 
 @pytest.fixture(scope='module')
@@ -35,11 +33,10 @@ def test_clone_into_empty_dir(tmpdir_gen):
     tmp_url = tmpdir_gen()
     tmp_path = tmpdir_gen()
     origin = legit.Repository.init(tmp_url)
-    cloned_repo = origin.clone(tmp_path)
-    assert cloned_repo.__class__ is Repo
-    assert Repo.init(tmp_url).__class__ is Repo
-    assert isdir(cloned_repo.working_tree_dir)
-    assert cloned_repo.git_dir.startswith(cloned_repo.working_tree_dir)
+    cloned_repo = origin.clone(url = tmp_url.strpath, path = tmp_path.strpath)
+    assert cloned_repo.__class__ is legit.Repository
+    assert isdir(cloned_repo.repo.working_tree_dir)
+    assert cloned_repo.repo.git_dir.startswith(cloned_repo.repo.working_tree_dir)
 
 
 def test_clone_into_non_empty_dir(tmpdir_gen):
@@ -47,50 +44,25 @@ def test_clone_into_non_empty_dir(tmpdir_gen):
     tmp_into = tmpdir_gen()
     legit.Repository.init(tmp_into)
     clone_from = legit.Repository.init(tmp_from)
-    new_clone = clone_from.clone(tmp_into)
+    new_clone = clone_from.clone(url = clone_from.repo.working_tree_dir, path = tmp_into.strpath)
     assert new_clone is None
 
 
-# TODO: Rewrite
 def test_checkout(tmpdir):
     repo = legit.Repository.init(tmpdir)
-    new_branch = repo.checkout('branch_name')
-    assert repo.active_branch == new_branch
+    repo.checkout('branch_name')
+    current_branch = repo.repo.active_branch.name
+    assert current_branch == 'branch_name'
 
 
-# TODO: Rewrite
 def test_add_file_and_commit(tmpdir):
     repo = legit.Repository.init(tmpdir)
-    with TemporaryFile(dir=tmpdir) as tmpfilename:
-        repo.add_changes(files=[tmpfilename])
-    assert repo.commit == repo.active_branch.commit
-
-# PLEASE DO NOT PUSH DEAD/COMMENTED-OUT CODE
-# def test_push():
-#     repository = legit.Repository.init(create_folder(folder)).repo
-#     repo = legit.Repository.init(folder).repo
-#     assert isdir(repository.working_tree_dir)
-#     assert repository.git_dir.startswith(repository.working_tree_dir)
-#     empty_repo = Repo.init(join(folder, 'empty'))
-#     origin = empty_repo.create_remote('origin', repo.remotes.origin.url)
-#     assert origin.exists()
-#     assert origin ==
-#             empty_repo.remotes.origin ==
-#            empty_repo.remotes['origin']
-#     # assure we actually have data. fetch() returns useful information
-#     origin.fetch()
-#     # Setup a local tracking branch of a remote branch
-#     # create local branch "master" from remote "master"
-#     empty_repo.create_head('master', origin.refs.master)
-#     # set local "master" to track remote "master
-#     empty_repo.heads.master.set_tracking_branch(origin.refs.master)
-#     # checkout local "master" to working tree
-#     empty_repo.heads.master.checkout()
-#     empty_repo.create_head('master', origin.refs.master)
-#                      .set_tracking_branch(origin.refs.master).checkout()
-#     # rename remotes
-#     origin.rename('new_origin')
-#     origin.pull()
-#     origin.push()
-#     # create and delete remotes
-#     assert not empty_repo.delete_remote(origin).exists()
+    with NamedTemporaryFile(dir=tmpdir) as tmpfile1, NamedTemporaryFile(dir=tmpdir) as tmpfile2:
+        tmpname1 = tmpfile1.name.split(sep)[-1]
+        tmpname2 = tmpfile2.name.split(sep)[-1]
+        assert len(repo.repo.untracked_files) == 2
+        assert tmpname1 in repo.repo.untracked_files
+        assert tmpname2 in repo.repo.untracked_files
+        repo.add_changes(files=[tmpname1])
+        assert len(repo.repo.untracked_files) == 1
+        assert list(repo.repo.index.entries.keys())[0][0] == tmpname1
