b1e4984f8aacfb6b348a9676516e2425940c15cf
Max Jekov <jekovmax@gmail.com>
2019-09-05 20:00:30 +0300

    mj-10: client reinit

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,15 +1,15 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import ControlPanel from '@/components/ControlPanel'
+import Vue from 'vue';
+import Router from 'vue-router';
+import HelloWorld from '@/components/HelloWorld';
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'ControlPanel',
-      component: ControlPanel
-    }
-  ]
-})
+      name: 'HelloWorld',
+      component: HelloWorld,
+    },
+  ],
+});
