335c090e29e1668a41f2ba935bede7e9886a43d3
Max Jekov <jekovmax@gmail.com>
2019-09-06 09:53:06 +0300

    mj-10: Add test output

--- client/src/components/ControlPanel.vue
+++ client/src/components/ControlPanel.vue
@@ -1,11 +1,36 @@
 <template>
-  
+  <div>
+      <p>{{ msg }}</p>
+  </div>
 </template>
 
 <script>
-export default {
+import axios from 'axios';
 
-}
+export default {
+  name: 'ControlPanel',
+  data() {
+    return {
+      msg: '',
+    };
+  },
+  methods: {
+    getAvailableSites() {
+      const path = 'http://localhost:5000/sites';
+      axios.get(path)
+        .then((res) => {
+          this.msg = res.data;
+        })
+        .catch((error) => {
+          // eslint-disable-next-line
+          console.error(error);
+        });
+    },
+  },
+  created() {
+    this.getAvailableSites();
+  },
+};
 </script>
 
 <style>
