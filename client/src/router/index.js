1087c18d975f2f29573fc4ec1aaed8052d791f89
Max Jekov <jekovmax@gmail.com>
2019-09-06 13:21:38 +0300

    mj-10: small fixes

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import ControlPanel from '@/components/ControlPanel';
-import CustomHeader from '@/components/CustomHeader';
 
 Vue.use(Router);
 
@@ -11,11 +10,6 @@ export default new Router({
       path: '/',
       name: 'ControlPanel',
       component: ControlPanel,
-    },
-    {
-      path: '/header',
-      name: 'CustomHeader',
-      component: CustomHeader,
     },
   ],
 });
