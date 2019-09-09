e82ebdd5219ab38749ef11100683802c2d084583
Max Jekov <jekovmax@gmail.com>
2019-09-09 13:51:18 +0300

    mj-10: change project structure

--- client/src/main.js
+++ client/src/main.js
@@ -1,19 +1,11 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import 'bootstrap/dist/css/bootstrap.css';
 import BootstrapVue from 'bootstrap-vue';
-import Vue from 'vue';
-import App from './App';
-import router from './router';
+import Vue from 'vue'
+import App from './App.vue'
 
-Vue.config.productionTip = false;
-
+Vue.config.productionTip = false
 Vue.use(BootstrapVue);
 
-/* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>',
-});
+  render: h => h(App),
+}).$mount('#app')
