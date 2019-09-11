6afbc72cffc5f0e57f027b27f7bc8dafe5a50b5b
Anton Patrushev <apatrushev@gmail.com>
2019-09-11 15:37:49 +0200

    requirements moved to setup.py

--- setup.py
+++ setup.py
@@ -38,6 +38,21 @@ setuptools.setup(
     extras_require={
         'dev': [
             'aiohttp-devtools',
+
+            'pydocstyle',
+            'pytest',
+            'pytest-cov',
+            'flake8',
+            'flake8-import-order-spoqa',
+            'flake8-docstrings',
+            'flake8-builtins',
+            'flake8-blind-except',
+            'flake8-quotes',
+            'flake8-eradicate',
+            'flake8-print',
+            'flake8-mutable',
+            'flake8-spellcheck',
+            'pep8-naming',
         ]
     },
     zip_safe=True,
