dd6d1b4920ce90a9fbd5eeab53f093b8a17dd35f
Anton Patrushev <apatrushev@gmail.com>
2019-09-11 17:33:11 +0200

    implemented some tests

---
+++ tests/test_grqphql.py
@@ -0,0 +1,66 @@
+from collections import OrderedDict
+from graphene import Schema
+from graphene.test import Client
+from lektorium.schema import Query, MutationQuery
+from lektorium.repo import ListRepo, SITES
+
+
+def deorder(obj):
+    '''Removes OrderedDict's in object tree'''
+    if isinstance(obj, (OrderedDict, dict)):
+        return {k: deorder(v) for k, v in obj.items()}
+    elif isinstance(obj, list):
+        return [deorder(k) for k in obj]
+    return obj
+
+
+def test_query_sites():
+    client = Client(Schema(
+        query=Query,
+        mutation=MutationQuery,
+    ), context={'repo': ListRepo(SITES)})
+    result = client.execute(r'''{
+        sites {
+            siteId
+        }
+    }''')
+    assert deorder(result) == {
+        'data': {
+            'sites': [
+                {'siteId': 'bow'},
+                {'siteId': 'uci'},
+                {'siteId': 'ldi'},
+            ]
+        }
+    }
+
+
+def test_destroy_session():
+    client = Client(Schema(
+        query=Query,
+        mutation=MutationQuery,
+    ), context={'repo': ListRepo(SITES)})
+    result = client.execute(r'''{
+        sessions(parked: true) {
+            sessionId
+        }
+    }''')
+    assert deorder(result) == {
+        'data': {
+            'sessions': [
+                {'sessionId': 'pantssss'},
+                {'sessionId': 'pantss1'},
+            ]
+        }
+    }
+    result = client.execute(r'''mutation {
+        destroySession(sessionId: "pantss1") {
+            ok
+        }
+    }''')
+    assert deorder(result) == {
+        'data': {
+            'destroySession': {
+            },
+        }
+    }
