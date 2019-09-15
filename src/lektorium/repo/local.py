a71d6c7e2e99b0336a53cf0846e2e165d0cb0bc2
Anton Patrushev <apatrushev@gmail.com>
2019-09-16 00:03:23 +0200

    implemented sites call of local repo

--- src/lektorium/repo/local.py
+++ src/lektorium/repo/local.py
@@ -1,13 +1,55 @@
+import collections.abc
+
+import bidict
+import yaml
+from cached_property import cached_property
+
 from .interface import Repo as BaseRepo
+
+
+class Site(collections.abc.Mapping):
+    ATTR_MAPPING = bidict.bidict({
+        'name': 'site_name',
+        'staging': 'staging_url',
+        'production': 'production_url',
+        'email': 'custodian_email',
+        'owner': 'custodian',
+    })
+
+    def __init__(self, site_id, **props):
+        self.data = dict(props)
+        self.data['site_id'] = site_id
+        if self.data.get('sessions') is not None:
+            raise RuntimeError()
+
+    def __getitem__(self, key):
+        return self.data[key]
+
+    def __iter__(self):
+        for k in self.data:
+            yield self.ATTR_MAPPING.get(k, k)
+        yield 'sessions'
+
+    def __len__(self):
+        return len(self.data)
 
 
 class Repo(BaseRepo):
     def __init__(self, repo_root_dir):
         self.repo_root_dir = repo_root_dir
 
+    @cached_property
+    def config(self):
+        with (self.repo_root_dir / 'config.yml').open('rb') as config_file:
+            config = yaml.load(config_file)
+            return {
+                site_id: Site(site_id, **props)
+                for site_id, props in config.items()
+            }
+
     @property
     def sites(self):
-        raise NotImplementedError()
+        yield from self.config.values()
 
     @property
     def sessions(self):
