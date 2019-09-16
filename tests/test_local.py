3c945afba1db692cde53aea723f5eef72dcefaec
Anton Patrushev <apatrushev@gmail.com>
2019-09-16 06:04:15 +0200

    local session creation

--- tests/test_local.py
+++ tests/test_local.py
@@ -3,6 +3,13 @@ from lektorium.repo import LocalRepo
 from lektorium.repo.local import FakeServer
 
 
+@pytest.fixture
+def repo(tmpdir):
+    repo = LocalRepo(tmpdir, FakeServer())
+    repo.create_site('bow', 'Buy Our Widgets')
+    return repo
+
+
 def test_fake_server():
     server = FakeServer()
     assert server.serve_lektor('/tmp')
@@ -20,3 +27,8 @@ def test_create_site(tmpdir):
     assert len(list(repo.sites)) == 1
     repo = LocalRepo(tmpdir, FakeServer())
     assert len(list(repo.sites)) == 1
+
+
+def test_session_create(repo):
+    repo.create_session(next(repo.sites)['site_id'])
+    assert len(list(repo.sessions)) == 1
