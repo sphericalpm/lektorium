28bf0af0804d94d3d0291b3b95a34ac24f834043
Anton Patrushev <apatrushev@gmail.com>
2019-09-17 03:27:47 +0200

    lektor serving in local backend

---
+++ tests/test_server.py
@@ -0,0 +1,41 @@
+import asyncio
+import tempfile
+import async_timeout
+import pytest
+from unittest.mock import MagicMock
+from lektorium.repo.local import AsyncLocalServer
+
+
+class AsyncTestServer(AsyncLocalServer):
+    START_PORT = 5000
+    END_PORT = 5000
+
+    def __init__(self, command, ):
+        super().__init__()
+        self.COMMAND = command
+
+
+@pytest.mark.asyncio
+async def test_start_server_failed():
+    result = AsyncTestServer('echo').serve_lektor('/tmp')
+    while callable(result):
+        await asyncio.sleep(0.1)
+        result = result()[0]
+    assert result == 'Failed to start'
+
+
+@pytest.mark.asyncio
+async def test_start_stop_server():
+    with tempfile.TemporaryDirectory() as tmp:
+        cmd = 'echo "Finished prune"; sleep 10'
+        server = AsyncTestServer(cmd)
+        result = server.serve_lektor(tmp)
+        while callable(result):
+            await asyncio.sleep(0.1)
+            result = result()[0]
+        assert result == 'http://localhost:5000'
+        finalizer = MagicMock()
+        server = server.stop_server(tmp, finalizer=finalizer)
+        async with async_timeout.timeout(2):
+            while not finalizer.call_count:
+                await asyncio.sleep(0.1)
