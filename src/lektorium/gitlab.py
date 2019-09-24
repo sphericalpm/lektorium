0ab2e724b42c275291791f328a5a4e30812ff8e1
Max <mj@spherical.pm>
2019-09-24 17:19:06 +0300

    mj-13: update function result returning

--- src/lektorium/gitlab.py
+++ src/lektorium/gitlab.py
@@ -10,12 +10,12 @@ class GitLab:
 
     def create_merge_request(
         self,
-        project_url,
-        source_branch,
-        target_branch,
-        title,
-        assignee
-    ):
+        project_url: str,
+        source_branch: str,
+        target_branch: str,
+        title: str,
+        assignee: str
+    ) -> bool:
         project_path = self.get_project_path(project_url)
         request_url = f'{self.gitlab_url}/api/v4/projects/{project_path}/merge_requests'
         assignee_id = self.get_user_id(assignee)
@@ -24,15 +24,18 @@ class GitLab:
                         'source_branch': source_branch,
                         'target_branch': target_branch,
                         'assignee_id': assignee_id}
-        response = requests.post(request_url, headers=self.headers, data=request_data).json()
-        return {'iid': response['iid'], 'merge_status': response['merge_status']}
+        response = requests.post(request_url, headers=self.headers, data=request_data)
+        if response.status_code == 201:
+            return True
+        else:
+            return False
 
-    def get_project_path(self, project_url):
+    def get_project_path(self, project_url: str) -> str:
         gitlab_url = self.gitlab_url + '/'
         project_path = project_url.replace(gitlab_url, '')
         return project_path.replace('/', '%2f')
 
-    def get_user_id(self, username):
+    def get_user_id(self, username: str) -> str:
         request_url = f'{self.gitlab_url}/api/v4/users?username={username}'
         response = requests.get(request_url, headers=self.headers).json()
         if response:
