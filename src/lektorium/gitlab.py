fb44db37fd1d81cbb8584e13373251e963f105d1
Max <mj@spherical.pm>
2019-09-24 12:13:55 +0300

    mj-13: Implement get user id func

--- src/lektorium/gitlab.py
+++ src/lektorium/gitlab.py
@@ -6,6 +6,7 @@ class GitLab:
     def __init__(self, gitlab_url):
         self.gitlab_url = gitlab_url
         self.auth_token = os.environ['GITLAB_TOKEN']
+        self.headers = {'Private-Token': self.auth_token}
 
     def create_merge_request(
         self,
@@ -17,8 +18,11 @@ class GitLab:
     ):
         pass
 
-    def get_project_path(project_url):
+    def get_project_path(self, project_url):
         pass
 
-    def get_user_id(username):
-        pass
+    def get_user_id(self, username):
+        request_url = f"{self.gitlab_url}/api/v4/users?username={username}"
+        response = requests.get(request_url, headers=self.headers).json()
+        if response:
+            return response[0]['id']
