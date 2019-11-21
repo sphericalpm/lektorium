b3cc9cda8f5ce49464dd5b26f8b4ac445df0c3b3
Max Jekov <jekovmax@gmail.com>
2019-11-21 22:11:07 +0200

    mj-iarcrp-38: implement auth management init

--- src/lektorium/auth0.py
+++ src/lektorium/auth0.py
@@ -1,6 +1,12 @@
 class AuthManagement:
-    def __init__(self, auth):
-        pass
+    def __init__(self, domain, client_id, client_secret):
+        self.url = f'https://{domain}/oauth/token'
+        self.data = {
+            'client_id': client_id,
+            'client_secret': client_secret,
+            'audience': f'https://{domain}/api/v2/',
+            'grant_type': 'client_credentials',
+        }
 
     def get_auth_token(self):
         pass
