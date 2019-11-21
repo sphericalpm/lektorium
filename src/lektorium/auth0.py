d8442982fb94d138ae9c02fbf9f53f92a0ff91f8
Max Jekov <jekovmax@gmail.com>
2019-11-21 21:43:26 +0200

    mj-iarcrp-38: add auth management skeleton

---
+++ src/lektorium/auth0.py
@@ -0,0 +1,12 @@
+class AuthManagement:
+    def __init__(self, auth):
+        pass
+
+    def get_auth_token(self):
+        pass
+
+    def get_users(self):
+        pass
+
+    def get_user_permissions(self):
+        pass
