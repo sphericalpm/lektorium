94e217edf16f896e618a31e57bc8bdf38946fe5d
Max Jekov <jekovmax@gmail.com>
2019-11-28 17:16:12 +0200

    mj-iarcrp-38: Add test for get_users func

--- tests/test_auth0_client.py
+++ tests/test_auth0_client.py
@@ -4,6 +4,14 @@ from lektorium.auth0 import Auth0Client, Auth0Error
 
 pytestmark = pytest.mark.asyncio
 
+TEST_TOKEN = {'access_token': 'test_token'}
+TEST_AUTH_DATA = {
+    'data-auth0-domain': 'testdomain',
+    'data-auth0-api': 'testapi',
+    'data-auth0-management-id': 'testid',
+    'data-auth0-management-secret': 'testsecret',
+}
+
 
 @pytest.fixture
 def auth0_client():
@@ -17,11 +25,24 @@ def auth0_client():
 
 
 async def test_auth_token(auth0_client):
-    test_token = {'access_token': 'test_token'}
     with aioresponses() as mocked:
-        mocked.post(auth0_client.url, status=200, payload=test_token)
+        mocked.post(auth0_client.url, status=200, payload=TEST_TOKEN)
         resp = await auth0_client.auth_token
         assert resp == 'test_token'
         mocked.post(auth0_client.url, status=404)
         with pytest.raises(Auth0Error):
             resp = await auth0_client.auth_token
+
+
+async def test_get_users(auth0_client):
+    with aioresponses() as mocked:
+        url = auth0_client.data["audience"] + 'users?fields=name,nickname,email,user_id'
+        users_response = []
+        mocked.post(auth0_client.url, status=200, payload=TEST_TOKEN)
+        mocked.get(url, status=200, payload=users_response)
+        resp = await auth0_client.get_users()
+        assert resp == []
+        mocked.post(auth0_client.url, status=200, payload=TEST_TOKEN)
+        mocked.get(url, status=400)
+        with pytest.raises(Auth0Error):
+            resp = await auth0_client.get_users()
