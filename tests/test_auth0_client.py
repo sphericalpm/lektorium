1de958e4e1625c0031cfe4e09fbb8fedcbf21c77
Max Jekov <jekovmax@gmail.com>
2019-11-29 08:55:11 +0200

    mj-iarcrp-38: improve test coverage

--- tests/test_auth0_client.py
+++ tests/test_auth0_client.py
@@ -100,3 +100,8 @@ async def test_get_api_permissions(auth0_client):
         mocked.get(url, status=400)
         with pytest.raises(Auth0Error):
             resp = await auth0_client.get_api_permissions()
+        permissions_response = []
+        mocked.post(auth0_client.url, status=200, payload=TEST_TOKEN)
+        mocked.get(url, status=200, payload=permissions_response)
+        with pytest.raises(Auth0Error):
+            resp = await auth0_client.get_api_permissions()
